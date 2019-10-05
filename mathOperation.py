40e161320b914b83488804a9bd530a41a8a4ea3f
Danica Washington <55327173+dvns17@users.noreply.github.com>
2019-10-05 10:24:03 -0500

    Initial commit

---
+++ mathOperation.py
@@ -0,0 +1,40 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+a = 2 + 3
+
+a = 2 
+
+b = 3
+
+c = b - a 
+
+b = "danica"
+
+c = "is awesome"
+
+d = b + " " + c 
+print(d)
+
+x = 2
+
+y = 3
+
+z = x * y
+print(z)
+
+z = x / y 
+print(z)
+
+z = x // y
+print(z)
+
+d = 20
+
+e = 6
+
+f = d // e 
+print("f equals " + str(f)) 
---
+++ mathOperationDrill.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+a = 6
+
+b = -18
+
+c = (a + b)
+print("c equals " + str(c)) 
+
+
+d = 8
+
+e = 94
+
+f = [d + e] 
+print (f) 
